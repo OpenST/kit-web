2725779a5adb9dddff69cdeda02499cd944e43b8
sunil <sunil@hem.com>
2019-03-04 16:06:28 +0530

    Initial commit for dashboard

--- app/controllers/web/economy_controller.rb
+++ app/controllers/web/economy_controller.rb
@@ -7,6 +7,27 @@ class Web::EconomyController < Web::BaseController
 
   after_action :remove_browser_caching
 
+
+  # Economy dashboard
+  #
+  # * Author: Sunil
+  # * Date: 01/03/2019
+  # * Reviewed By: Puneet
+  #
+  def dashboard
+    @response = CompanyApi::Request::Token.new(
+      CompanyApi::Response::Formatter::Token,
+      request.cookies,
+      {"User-Agent" => http_user_agent}
+    ).fetch_token_details()
+
+    unless @response.success?
+      return handle_temporary_redirects(@response)
+    end
+
+    @presenter_obj = ::WebPresenter::TokenPresenter.new(@response, params)
+  end
+
   # Planner to perform economy setup step one
   #
   # * Author: Ankit
--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,7 @@ Rails.application.routes.draw do
   end
 
   scope "#{GlobalConstant::Environment.url_prefix}", controller: 'web/economy' do
+    get '/' => :dashboard, as: 'dashboard'
     get '/token/setup' => :token_setup, as: 'token_setup'
     get '/token/deploy' => :token_deploy, as: 'token_deploy'
     get '/token/mint' => :token_mint, as: 'token_mint'

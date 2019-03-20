6c52f36ff8d79063871864d970ce32de4c658e45
Kedar Chandrayan <kedarchandrayan@gmail.com>
2019-03-21 02:14:04 +0530

    code review.

--- lib/global_constant/cookie.rb
+++ lib/global_constant/cookie.rb
@@ -6,16 +6,16 @@ module GlobalConstant
     class << self
 
       def user_cookie_name
-        'ost_kit_ca'
+        'ost_platform_ca'
       end
 
       def last_used_env_cookie_name
-        'ost_kit_luse'
+        'ost_platform_luse'
       end
 
       # Not a actual cookie. but key is used internally to manage cookies instructed by company API
       def new_api_cookie_key
-        'ost_kit_new_api_cookies'
+        'ost_platform_new_api_cookies'
       end
 
     end

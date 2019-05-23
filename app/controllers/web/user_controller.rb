a52f6c78d4f0d3edf58c4d80ee8ccc63d54843ba
AnkitSingh <ankit@ost.com>
2019-05-23 16:13:00 +0530

    changes related to verify device api.

--- app/controllers/web/user_controller.rb
+++ app/controllers/web/user_controller.rb
@@ -181,6 +181,33 @@ class Web::UserController < Web::BaseController
     
   end
 
+  def verify_device
+    unless Util::CommonValidator.is_valid_token?(params[:d_t])
+      render 'web/user/invalid_token'
+      return
+    end
+
+    @response = CompanyApi::Request::Access.new(
+      CompanyApi::Response::Formatter::Manager,
+      request.cookies,
+      {"User-Agent" => http_user_agent}
+    ).verify_device(d_t: params[:d_t])
+
+    if @response.success?
+      if @response.go_to.present?
+        handle_temporary_redirects(@response)
+        return
+      else
+        render 'web/user/verify_email_success' and return
+      end
+    elsif @response.http_code == GlobalConstant::ErrorCode.unauthorized_access
+      redirect_to :login and return
+    else
+      render 'web/user/invalid_token'
+      return
+    end
+  end
+
   
   
 end

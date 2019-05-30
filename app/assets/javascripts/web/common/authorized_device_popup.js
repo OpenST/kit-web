4c9717e826a9d309884a0fb372b7bf020bd0d990
Shraddha <shraddha.patil@ost.com>
2019-05-30 13:05:15 +0530

    minor changes

--- app/assets/javascripts/web/common/authorized_device_popup.js
+++ app/assets/javascripts/web/common/authorized_device_popup.js
@@ -1,7 +1,16 @@
 ;(function (window,$) {
-  if(true){
-    setTimeout(function () {
-      $(".authorized_device_popup").animate({opacity:0})
-    },5000)
+  var ost = ns('ost'),
+      authorizeDevice = ns('ost.authorizeDevice')
+  ;
+
+  var oThis = ost.authorizeDevice ={
+    init: function () {
+      if(true){
+        setTimeout(function () {
+          $(".authorized_device_popup").animate({opacity:0})
+        },5000)
+      }
+    }
   }
+
 })(window,jQuery);

2bf7ed319d8b3e3e25d589cc0d18d57cd224d3d1
Shraddha <shraddha.patil@ost.com>
2019-06-03 20:44:10 +0530

    minor fix

--- app/assets/javascripts/web/common/authorized_device_popup.js
+++ app/assets/javascripts/web/common/authorized_device_popup.js
@@ -1,14 +1,15 @@
 ;(function (window,$) {
   var ost = ns('ost'),
-      authorizeDevice = ns('ost.authorizeDevice')
+      authorizeDevice = ns('ost.authorizeDevice'),
+      timeOut = 3000
   ;
 
   var oThis = ost.authorizeDevice ={
     init: function (config) {
       if(config.devtCookiePresent){
-        setTimeout(function () {
-          $(".authorized_device_popup").animate({opacity:0})
-        },3000)
+        setTimeout( function () {
+          $(".authorized_device_popup").animate({'opacity':0})
+        }, timeOut );
       }
     }
   }

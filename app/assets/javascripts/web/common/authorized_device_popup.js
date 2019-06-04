9dae9f62c056767ac0be8ab0dd29c646542e9347
Shraddha <shraddha.patil@ost.com>
2019-06-04 12:30:16 +0530

    timeout fix

--- app/assets/javascripts/web/common/authorized_device_popup.js
+++ app/assets/javascripts/web/common/authorized_device_popup.js
@@ -1,15 +1,17 @@
 ;(function (window,$) {
   var ost = ns('ost'),
       authorizeDevice = ns('ost.authorizeDevice'),
-      timeOut = 3000
+      timeout = "3000"
   ;
 
   var oThis = ost.authorizeDevice ={
     init: function (config) {
       if(config.devtCookiePresent){
+        console.log("parseInt(timeout)",parseInt(timeout));
+        console.log("timeout",timeout);
         setTimeout( function () {
           $(".authorized_device_popup").animate({'opacity':0})
-        }, timeOut );
+        }, parseInt(timeout) );
       }
     }
   }

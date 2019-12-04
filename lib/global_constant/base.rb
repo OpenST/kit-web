bb5e277f365175dd08b0a61b731375b121b1a5cd
Pankaj Keswani <pankaj@Pankajs-MacBook-Pro.local>
2019-12-04 13:29:14 +0530

    Forking support

--- lib/global_constant/base.rb
+++ lib/global_constant/base.rb
@@ -58,6 +58,10 @@ module GlobalConstant
         @stw_campaign_details ||= fetch_config.fetch('pepo_campaign', {}).with_indifferent_access
       end
 
+      def is_forking_in_progress?
+        @forking_in_progress ||= (fetch_config.fetch('forking_in_progress', 0) == 1)
+      end
+
       def is_public_launch_done?
         true
       end
